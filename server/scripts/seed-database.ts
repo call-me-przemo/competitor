702e6c16b1ebd31f8ebb8588f00230b9dd899aca
Przemysław Mielniczek <pmiel13@gmail.com>
2023-11-08 23:20:28 +0100

    fix(server): database seeder

--- server/scripts/seed-database.ts
+++ server/scripts/seed-database.ts
@@ -1,26 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import { Seeder } from "../prisma/seeder";
 
+export const prisma = new PrismaClient();
+
 (async () => {
-  const prisma = new PrismaClient();
   const seeder = new Seeder(prisma);
-  const personCount = 10_000;
-  const userCount = 3_000;
-  const organizationCount = 120;
-  const organizationMembersCount = 400;
-  const competitionCount = 500;
-  const timersCount = 14;
-  const competitionTimersCount = 1_200;
-  const disciplineCount = 1_800;
-  const participantCount = 200_000;
+  const personCount = 1_000;
+  const userCount = 300;
+  const organizerCount = 20;
+  const timerCount = 8;
+  const organizationCount = 8;
+  const organizationMembersCount = 3;
+  const competitionCount = 50;
+  const competitionTimersCount = 3;
+  const disciplineCount = 3;
+  const participantCount = 50;
 
   await seeder.seedPerson(personCount);
-  await seeder.seedUser(userCount, organizationMembersCount, timersCount);
+  await seeder.seedUser(userCount, organizerCount, timerCount);
   await seeder.seedOrganization(organizationCount);
   await seeder.seedOrganizationMember(organizationMembersCount);
   await seeder.seedCompetition(competitionCount);
   await seeder.seedCompetitionTimer(competitionTimersCount);
   await seeder.seedDiscipline(disciplineCount);
   await seeder.seedParticipant(participantCount);
-  await prisma.$disconnect();
 })();
