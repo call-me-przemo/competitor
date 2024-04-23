bee8c69429e5ed5778f503abce3a00413c954968
Przemysław Mielniczek <pmiel13@gmail.com>
2024-04-23 14:03:20 +0200

    fix(server): fastify bootstraping

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,23 +1,10 @@
-import Fastify from 'fastify';
+import { fastify } from 'fastify';
 import { app } from './app/app';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-// Instantiate Fastify with some config
-const server = Fastify({
-  logger: true,
-});
-
-// Register your application as a normal plugin.
-server.register(app);
-
-// Start listening.
-server.listen({ port, host }, (err) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  } else {
-    console.log(`[ ready ] http://${host}:${port}`);
-  }
-});
+fastify({
+  logger: {
+    level: process.env.LOG_LEVEL,
+  },
+})
+  .register(app)
+  .listen({ port: Number(process.env.APP_PORT), host: process.env.APP_HOST });
