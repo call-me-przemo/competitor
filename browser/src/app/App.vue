adce618233715b701765e7fd966f05eda294377a
Przemysław Mielniczek <pmiel13@gmail.com>
2024-04-30 11:33:54 +0200

    feat(browser): add navbar

--- browser/src/app/App.vue
+++ browser/src/app/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
+  <nav>
+    <Navbar />
+  </nav>
   <main>
     <RouterView />
   </main>
