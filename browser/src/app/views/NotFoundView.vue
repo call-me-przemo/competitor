013eb382f7ea880383121799b3a019a00956ad03
Przemysław Mielniczek <pmiel13@gmail.com>
2024-04-30 14:09:47 +0200

    feat(browser): add not found view

---
+++ browser/src/app/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useTitle } from '@vueuse/core';
+
+useTitle('404 | Competitor');
+</script>
+<template>
+  <div
+    class="flex min-h-screen text-teal-300 align-items-center justify-content-center"
+  >
+    <h1 class="font-italic text-2xl">Not found</h1>
+  </div>
+</template>
