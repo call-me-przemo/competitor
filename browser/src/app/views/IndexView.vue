b045f23068db3a73f95c8259eabb9803bb61bdd9
Przemysław Mielniczek <pmiel13@gmail.com>
2024-06-07 14:14:31 +0200

    feat(browser): add index view

--- browser/src/app/views/IndexView.vue
+++ browser/src/app/views/IndexView.vue
@@ -1,4 +1,60 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Galleria from 'primevue/galleria';
+
+const images = [
+  {
+    src: '/images/braden-collum-9HI8UJMSdZA-unsplash.jpg',
+    alt: 'braden-collum-9HI8UJMSdZA-unsplash',
+  },
+  {
+    src: '/images/brian-erickson-XFneC_rHR48-unsplash.jpg',
+    alt: 'brian-erickson-XFneC_rHR48-unsplash',
+  },
+  {
+    src: '/images/chander-r-AtfA8NDgpKA-unsplash.jpg',
+    alt: 'chander-r-AtfA8NDgpKA-unsplash',
+  },
+  {
+    src: '/images/chander-r-z4WH11FMfIQ-unsplash.jpg',
+    alt: 'chander-r-z4WH11FMfIQ-unsplash',
+  },
+  {
+    src: '/images/chermiti-mohamed-FdsqHM4fawg-unsplash.jpg',
+    alt: 'chermiti-mohamed-FdsqHM4fawg-unsplash',
+  },
+  {
+    src: '/images/lucas-favre-JnoNcfFwrNA-unsplash.jpg',
+    alt: 'lucas-favre-JnoNcfFwrNA-unsplash',
+  },
+  {
+    src: '/images/martins-zemlickis-NPFu4GfFZ7E-unsplash.jpg',
+    alt: 'martins-zemlickis-NPFu4GfFZ7E-unsplash',
+  },
+];
+</script>
 <template>
-  <div class="">Hi</div>
+  <section class="flex align-items-center justify-content-center">
+    <Galleria
+      :value="images"
+      :circular="true"
+      :showItemNavigators="true"
+      :showThumbnails="false"
+      :showItemNavigatorsOnHover="true"
+      :showIndicators="true"
+      :autoPlay="true"
+      :transitionInterval="2000"
+      :showIndicatorsOnItem="true"
+      :pt="{
+        root: {
+          class: 'm-2 lg:m-4 border-1 border-primary border-round xl:w-6',
+        },
+        previousItemIcon: { class: 'text-primary' },
+        nextItemIcon: { class: 'text-primary' },
+      }"
+    >
+      <template #item="slotProps">
+        <img :src="slotProps.item.src" :alt="slotProps.item.alt" />
+      </template>
+    </Galleria>
+  </section>
 </template>
