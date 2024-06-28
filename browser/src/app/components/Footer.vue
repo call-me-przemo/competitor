7d7f19a50537a5b090056aae3ecf5f9a766ac853
Przemysław Mielniczek <pmiel13@gmail.com>
2024-06-28 19:26:48 +0200

    feat(browser): change theme

--- browser/src/app/components/Footer.vue
+++ browser/src/app/components/Footer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="text-center p-2 text-lg border-top-1 border-primary">
+  <div class="text-center p-2 text-lg bg-primary">
     Competitor {{ new Date().getFullYear() }}
   </div>
 </template>
