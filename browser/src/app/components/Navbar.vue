3e39c89adb6e718d25cb68894284e3476d3dc117
Przemysław Mielniczek <pmiel13@gmail.com>
2024-07-14 18:45:42 +0200

    feat(browser): migrate to primevue-v4

--- browser/src/app/components/Navbar.vue
+++ browser/src/app/components/Navbar.vue
@@ -7,17 +7,10 @@ import { RouterLink } from 'vue-router';
 const items = ref<MenuItem[]>([]);
 </script>
 <template>
-  <Menubar
-    :model="items"
-    :pt="{
-      root: {
-        class: 'border-noround bg-primary',
-      },
-    }"
-  >
+  <Menubar :model="items" class="bg-primary rounded-none">
     <template #start>
-      <RouterLink :to="{ name: 'index' }" class="flex align-items-center">
-        <Image src="/icon.png" alt="app logo" class="mr-2 w-3rem" />
+      <RouterLink :to="{ name: 'index' }" class="flex items-center">
+        <Image src="/icon.png" alt="app logo" class="mr-2 w-14" />
         <p class="text-4xl">Competitor</p>
       </RouterLink>
     </template>
--- browser/src/main.ts
+++ browser/src/main.ts
@@ -2,10 +2,41 @@ import router from './app/router';
 import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
 import App from './app/App.vue';
-
+import Aura from '@primevue/themes/aura';
+import { definePreset } from '@primevue/themes';
 import './assets/main.css';
-import 'primevue/resources/themes/aura-dark-noir/theme.css';
-import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 
-createApp(App).use(router).use(PrimeVue, { ripple: true }).mount('#root');
+const preset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{orange.50}',
+      100: '{orange.100}',
+      200: '{orange.200}',
+      300: '{orange.300}',
+      400: '{orange.400}',
+      500: '{orange.500}',
+      600: '{orange.600}',
+      700: '{orange.700}',
+      800: '{orange.800}',
+      900: '{orange.900}',
+      950: '{orange.950}',
+    },
+  },
+});
+
+createApp(App)
+  .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset,
+      ripple: true,
+      options: {
+        cssLayer: {
+          name: 'primevue',
+          order: 'tailwind-base, primevue, tailwind-utilities',
+        },
+      },
+    },
+  })
+  .mount('#root');
