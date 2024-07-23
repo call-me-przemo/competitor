27eb341737a6394819ab642e1fa97a5e49fc2191
Przemysław Mielniczek <pmiel13@gmail.com>
2024-07-23 20:14:52 +0200

    feat(browser): add primevue global config

--- browser/src/app/components/Footer.vue
+++ browser/src/app/components/Footer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="text-center p-2 text-lg bg-primary">
+  <div class="text-center p-2 text-lg bg-primary-500">
     Competitor {{ new Date().getFullYear() }}
   </div>
 </template>
