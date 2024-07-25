bc8853e5c2ea58c8ace0a9701aeaafd33072f35a
Przemysław Mielniczek <pmiel13@gmail.com>
2024-07-26 00:23:44 +0200

    feat(browser): style navbar responsive menu

--- browser/src/app/components/Navbar.vue
+++ browser/src/app/components/Navbar.vue
@@ -4,15 +4,39 @@ import { MenuItem } from 'primevue/menuitem';
 import Image from 'primevue/image';
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
-const items = ref<MenuItem[]>([]);
+const items = ref<MenuItem[]>([
+  {
+    label: 'Example',
+    url: 'https://example.com',
+  },
+  {
+    label: 'Also example',
+    url: 'https://example.com',
+  },
+]);
 </script>
 <template>
   <Menubar
     :model="items"
-    class="rounded-none border-0 shadow-xl dark:shadow-gray-700"
+    class="rounded-none border-0 shadow-xl dark:shadow-gray-700 bg-primary-500"
+    :pt="{
+      rootList: {
+        class: 'ml-auto text-lg lg:shadow-none shadow-xl dark:shadow-gray-700',
+      },
+      itemContent: {
+        class:
+          'bg-inherit hover:text-primary-500 lg:hover:text-primary-contrast',
+      },
+      button: {
+        class: 'ml-auto bg-inherit',
+      },
+      buttonIcon: {
+        class: 'text-color size-10',
+      },
+    }"
   >
     <template #start>
-      <RouterLink :to="{ name: 'index' }" class="flex items-center">
+      <RouterLink :to="{ name: 'index' }" class="flex">
         <Image src="/icon.png" alt="app logo" class="mr-2 w-10" />
         <p class="text-3xl">Competitor</p>
       </RouterLink>
